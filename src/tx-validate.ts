984ed65f55d78827e5993b01e39a13c82cde96c8
Igor Suvorov <hi@isuvorov.com>
2024-02-08 14:59:47 +0300

    feat: version based on @demensdeum

--- src/tx-validate.ts
+++ src/tx-validate.ts
@@ -1,11 +1,49 @@
 import type { Result, Transaction, UserBalance } from './types';
 
 export function txValidate(transactions: Transaction[], initBalance: UserBalance = 0): Result {
+  let balance = initBalance;
+  const sorted = transactions.sort((a, b) => (a.id > b.id ? 1 : -1));
+  const invalidOrderIds = new Set<number>();
+
+  const res = sorted.map((e) => {
+    if (e.txType === 'Bet') {
+      balance -= e.amount;
+    } else {
+      balance += e.amount;
+    }
+
+    if (balance < 0) {
+      invalidOrderIds.add(e.orderId);
+      return {
+        ...e,
+        valid: false,
+      };
+    }
+
+    if (invalidOrderIds.has(e.orderId)) {
+      return {
+        ...e,
+        valid: false,
+      };
+    }
+
+    // eslint-disable-next-line no-shadow
+    const transactionIds = sorted.map((e) => e.id);
+    if (transactionIds.filter((id) => id === e.id).length > 1) {
+      return {
+        ...e,
+        valid: false,
+      };
+    }
+
+    return {
+      ...e,
+      valid: true,
+    };
+  });
+
   return {
-    transactions: transactions.map((tx, i) => ({
-      ...tx,
-      valid: i % 2 === 0,
-    })),
-    balance: initBalance * 2,
+    transactions: res,
+    balance,
   };
 }
