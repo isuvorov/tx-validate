73527ec341a621e4e2c7f2fb5ecbd388b36d26ff
Igor Suvorov <hi@isuvorov.com>
2024-02-08 14:50:11 +0300

    feat: version based on @newyorrker

--- src/tx-validate.ts
+++ src/tx-validate.ts
@@ -1,11 +1,46 @@
 import type { Result, Transaction, UserBalance } from './types';
 
 export function txValidate(transactions: Transaction[], initBalance: UserBalance = 0): Result {
+  let accountBalance = initBalance;
+  const transactionIds = new Set<number>();
+  const invalidTransactionsOrderIds = new Set<number>();
+  const sortedTransaction = transactions.sort((a, b) => a.id - b.id);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const transaction of sortedTransaction) {
+    if (transactionIds.has(transaction.id)) {
+      // @ts-ignore
+      transaction.valid = false;
+    }
+
+    if (invalidTransactionsOrderIds.has(transaction.orderId)) {
+      // @ts-ignore
+      transaction.valid = false;
+    }
+
+    // @ts-ignore
+    if (transaction.valid) {
+      if (transaction.txType === 'Bet') {
+        accountBalance -= transaction.amount;
+      }
+
+      if (transaction.txType === 'Win') {
+        accountBalance += transaction.amount;
+      }
+    }
+
+    if (accountBalance < 0) {
+      // @ts-ignore
+      transaction.valid = false;
+      invalidTransactionsOrderIds.add(transaction.id);
+    }
+
+    transactionIds.add(transaction.id);
+  }
+
   return {
-    transactions: transactions.map((tx, i) => ({
-      ...tx,
-      valid: i % 2 === 0,
-    })),
-    balance: initBalance * 2,
+    // @ts-ignore
+    transactions: sortedTransaction,
+    balance: accountBalance,
   };
 }
